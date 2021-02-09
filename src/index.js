64467f63873d465bf30e9984e8312bfa5f64acac
Quang Tran <quang.tran290699@gmail.com>
2021-02-10 04:02:31 +0700

    --no commit message

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.js';
 import { Provider } from 'react-redux';
 import store from './stores/store';
 import { icons } from './asset/icon';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import './i18n/i18n';
-// import 'ag-grid-enterprise';
+import '@ui5/webcomponents/dist/Assets.js';
+import '@ui5/webcomponents-react/dist/Assets.js';
+import '@ui5/webcomponents-fiori/dist/Assets.js';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import './i18n/i18n';
 
 const loading = (
   <div className="pt-3 text-center">
     <div className="sk-spinner sk-spinner-pulse"></div>
   </div>
 );
-
 
 React.icons = icons;
 
