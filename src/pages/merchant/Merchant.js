15619f5f278c00de6cb9cb167f93d2cd72aa68cc
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-07-17 09:32:21 +0700

    change merchant

--- src/pages/merchant/Merchant.js
+++ src/pages/merchant/Merchant.js
@@ -48,31 +48,138 @@ function Merchant(props) {
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Id</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Code</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Name</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Username</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Phone</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Email</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Address</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Tax Code</StyledTableCell>
-              <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>Action</StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Id
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Code
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Tên
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Username
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Điện thoại
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Email
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Địa chỉ
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Mã số thuế
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ border: '1px solid #d4d3d3', boxSizing: 'border-box' }}
+              >
+                Hành động
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {merchants.map(row => (
-              <StyledTableRow key={row.id} style={{boxSizing: 'border-box'}}>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}} component="th" scope="row">{row.id}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.code}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.name}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.username}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.phone}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.email}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.address}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>{row.taxCode}</StyledTableCell>
-                <StyledTableCell style={{border: '1px solid #d4d3d3', boxSizing: 'border-box'}}>
-                 <CButton size="sm" className="btn-facebook btn-brand mr-1" onClick={() => props.history.push(`merchant/${row.id}`)}>View</CButton>
-                 <CButton size="sm" className="btn-youtube btn-brand mr-1">Delete</CButton>
+              <StyledTableRow key={row.id} style={{ boxSizing: 'border-box' }}>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                  component="th"
+                  scope="row"
+                >
+                  {row.id}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.code}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.username}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.phone}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.email}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.address}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  {row.taxCode}
+                </StyledTableCell>
+                <StyledTableCell
+                  style={{
+                    border: '1px solid #d4d3d3',
+                    boxSizing: 'border-box'
+                  }}
+                >
+                  <CButton
+                    size="sm"
+                    className="btn-facebook btn-brand mr-1 mb-1"
+                    onClick={() => props.history.push(`merchant/${row.id}`)}
+                  >
+                    Xem
+                  </CButton>
+                  <CButton size="sm" className="btn-youtube btn-brand mr-1">
+                    Xóa
+                  </CButton>
                 </StyledTableCell>
               </StyledTableRow>
             ))}
